915b6fb26a5e0b9264a22ac771d09d54c665b29f
Lee Young Joon <80800677+LYJ0304@users.noreply.github.com>
2026-06-14 21:42:49 +0900

    [Passed] 두 배열의 곱 (48ms, 16MB)

---
+++ trail0/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,16 @@
+a = [
+    list(map(int, input().split()))
+    for _ in range(3)
+]
+
+aa = input()
+
+b = [
+    list(map(int, input().split()))
+    for _ in range(3)
+]
+
+for i in range(3):
+    for j in range(3):
+        print(a[i][j] * b[i][j], end=" ")
+    print()

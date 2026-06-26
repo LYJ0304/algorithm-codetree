0032c9c0a459bf508a210e5b90833b4119f9d1cd
Lee Young Joon <80800677+LYJ0304@users.noreply.github.com>
2026-06-26 20:30:05 +0900

    [Passed] 두 정수 값 교환하기 (49ms, 16MB)

---
+++ trail2/두 정수 값 교환하기/to-exchange-two-integer-values.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def change_n(n, m):
+    n = m
+    return n
+
+def change_m(n, m):
+    m = n
+    return m
+
+print(change_n(n, m), change_m(n, m))

3473d29e7bb107a5acb8d9096ca6b899c7ece670
Lee Young Joon <80800677+LYJ0304@users.noreply.github.com>
2026-09-11 09:48:54 +0900

    [Passed] 총점 비교 (52ms, 16MB)

---
+++ trail2/총점 비교/compare-total-points.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+name = []
+score1 = []
+score2 = []
+score3 = []
+
+class Student:
+    def __init__(self, name, score1, score2, score3):
+        self.name = name
+        self.score1 = score1
+        self.score2 = score2
+        self.score3 = score3
+
+for _ in range(n):
+    student_input = input().split()
+    name.append(student_input[0])
+    score1.append(int(student_input[1]))
+    score2.append(int(student_input[2]))
+    score3.append(int(student_input[3]))
+
+# Please write your code here.
+students = []
+
+for i in range(n):
+    students.append(Student(name[i], score1[i], score2[i], score3[i]))
+
+students.sort(key=lambda x: (x.score1 + x.score2 + x.score3))
+
+for student in students:
+    print(student.name, student.score1, student.score2, student.score3)
